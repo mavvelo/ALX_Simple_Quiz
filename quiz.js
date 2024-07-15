3e4dfc18538e1cb7d679958f12f78516d75521c8
Isaac Cobby Andoh <cobbjy@gmail.com>
2024-07-15 05:35:36 -0400

    Java quiz updates

---
+++ quiz.js
@@ -0,0 +1,26 @@
+// function to check the user's answer
+function checkAnswer() {
+    // Correct answer
+    const correctAnswer = "4";
+
+    //Retrieve user's answer
+    const selectAnswer = document.querySelector('Input[name="quiz"]:checked');
+
+    // If no option is selected, do nothing 
+    if (!selectedAnswer) {
+        document.getElementById('feedback'.textContent = "Please select an answer");
+        return;
+    }
+
+    const userAnswer = selectAnswer.ariaValueMax;
+
+    // compare user's answer with the correct answer 
+    if (userAnswer === correctAnswer) {
+        document.getElementById('feedback').textContent = "Correct! Well done.";
+    } else {
+        document.getElementById('feedback').textContent = "That's incorrect. Try again!";
+    }
+}
+
+// Add event listener to the submit button
+document.getElementById('submit-answer').addEventListener('Click',checkAnswer);
