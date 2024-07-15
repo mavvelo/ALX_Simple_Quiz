fcd07d12cc3f6eb844e33b65d55628a8bb269d2a
Isaac Cobby Andoh <cobbjy@gmail.com>
2024-07-15 06:00:31 -0400

    Java quiz updates

--- calculator.js
+++ calculator.js
@@ -3,15 +3,15 @@ function add(number1, number2) {
     return number1 + number2
 }
 
-function subtract(number1. number2) {
+function subtract(number1, number2) {
     return number1 - number2;
 }
 
-function multiply(number1. number2) {
+function multiply(number1, number2) {
     return number1 * number2;
 }
 
-function divide(number1. number2) {
+function divide(number1, number2) {
     if (number2 === 0) {
         return 'Error: Division by zero';
     }
@@ -19,6 +19,7 @@ function divide(number1. number2) {
 }
 
 // Event listener functions
+
 document.getElementById('add').addEventListener('click', function() {
     const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
